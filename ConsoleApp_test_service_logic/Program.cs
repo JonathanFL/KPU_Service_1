e8af60f6eb3857532c28ca9aee92bbdc2df62f69
JonathanFL <j.lydemann@gmail.com>
2020-06-05 17:21:50 +0200

    Add project files.

---
+++ ConsoleApp_test_service_logic/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_test_service_logic
+{
+    class Program
+    {
+        private static string filePathToObserve = "C:\\Windows\\Temp\\KPU\\";
+        private static string filePathToAddContent = "C:\\Users\\Jonathan\\Documents\\TEMP_KPU\\KPU_service_lab_main_file.txt";
+
+        private static bool started;
+        static void Main(string[] args)
+        {
+            using (FileSystemWatcher fileSystemWatcher1 = new FileSystemWatcher())
+            {
+
+                Console.WriteLine("Starting using FileSystemWatcher");
+                started = true;
+                Task t = Task.Factory.StartNew(() =>
+                {
+                    
+                    fileSystemWatcher1.Path = filePathToObserve;
+
+                    // Watch for changes in LastAccess and LastWrite times, and
+                    // the renaming of files or directories.
+                    fileSystemWatcher1.NotifyFilter = NotifyFilters.LastAccess
+                                                      | NotifyFilters.LastWrite
+                                                      | NotifyFilters.FileName
+                                                      | NotifyFilters.DirectoryName;
+
+                    // Only watch text files.
+                    fileSystemWatcher1.Filter = "*.txt";
+
+                    // Add event handlers.
+                    fileSystemWatcher1.Created += FileSystemWatcher1Changed;
+                    fileSystemWatcher1.Renamed += FileSystemWatcher1Renamed;
+
+                    // Begin watching.
+                    fileSystemWatcher1.EnableRaisingEvents = true;
+
+                    var key = Console.ReadLine();
+                    if (key.ToLower().Equals("stop"))
+                    {
+                        started = false;
+                    }
+                });
+
+                while (started)
+                {
+                    
+                }
+
+                
+
+                Console.WriteLine("End Using FileSystemWatcher");
+                Console.ReadLine();
+            }
+
+
+
+            void FileSystemWatcher1Changed(object sender, FileSystemEventArgs e)
+            {
+                Console.WriteLine(e.Name + ", " + e.FullPath);
+                if (!File.Exists(e.FullPath)) return;
+
+                var contentOfFile = File.ReadAllText(e.FullPath);
+
+                File.WriteAllText(filePathToAddContent, contentOfFile);
+                File.WriteAllText("C:\\Users\\Jonathan\\Desktop\\KPU_SERVICE_DEBUG_FILE.txt", @"Came to end of fileSystemWatcher1_Changed");
+            }
+
+            void FileSystemWatcher1Renamed(object sender, FileSystemEventArgs e)
+            {
+                File.WriteAllText("C:\\Users\\Jonathan\\Desktop\\KPU_SERVICE_DEBUG_FILE.txt", @"fileSystemWatcher1_Renamed");
+            }
+        }
+    }
+}
---
+++ KPU_Service_1/Service1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KPU_Service_1
+{
+    public partial class KPU_SERVICE : ServiceBase
+    {
+        private string filePathToObserve = "C:\\Windows\\Temp\\KPU\\";
+        private string filePathToAddContent = "C:\\Users\\Jonathan\\Documents\\TEMP_KPU\\KPU_service_lab_main_file.txt";
+        private bool _started;
+        public KPU_SERVICE()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            _started = true;
+            Task.Factory.StartNew(() =>
+            {
+                using (fileSystemWatcher1 = new FileSystemWatcher())
+                {
+
+                    if (!EventLog.SourceExists("KPU_SERVICE"))
+                    {
+                        EventLog.CreateEventSource("KPU_SERVICE", "Application");
+                    }
+
+                    LogEvent("KPU_SERVICE started " + DateTime.Now, EventLogEntryType.Information);
+
+                    fileSystemWatcher1.Path = filePathToObserve;
+
+                    // Watch for changes in LastAccess and LastWrite times, and
+                    // the renaming of files or directories.
+                    fileSystemWatcher1.NotifyFilter = NotifyFilters.LastAccess
+                                                      | NotifyFilters.LastWrite
+                                                      | NotifyFilters.FileName
+                                                      | NotifyFilters.DirectoryName;
+
+                    // Only watch text files.
+                    fileSystemWatcher1.Filter = "*.txt";
+
+                    // Add event handlers.
+                    fileSystemWatcher1.Created += fileSystemWatcher1_Changed;
+                    fileSystemWatcher1.Renamed += fileSystemWatcher1_Renamed;
+
+                    // Begin watching.
+                    fileSystemWatcher1.EnableRaisingEvents = true;
+
+
+                    while (_started)
+                    {
+                        LogEvent("Service running", EventLogEntryType.Information);
+                        Thread.Sleep(1000);
+                    }
+
+                }
+
+                
+
+            });
+
+
+
+        }
+
+        protected override void OnStop()
+        {
+            _started = false;
+            LogEvent("Service stopped", EventLogEntryType.Information);
+        }
+
+        private void fileSystemWatcher1_Changed(object sender, FileSystemEventArgs e)
+        {
+            LogEvent("There has been created a file in the observed directory: " + e.FullPath, EventLogEntryType.Information);
+            if (!File.Exists(e.FullPath)) return;
+
+            var contentOfFile = File.ReadAllText(e.FullPath);
+
+            File.WriteAllText(filePathToAddContent, contentOfFile);
+            
+        }
+
+        private void fileSystemWatcher1_Renamed(object sender, FileSystemEventArgs e)
+        {
+            LogEvent("Renamed event " + e.FullPath, EventLogEntryType.Information);
+        }
+
+        private void LogEvent(string message, EventLogEntryType entryType)
+        {
+            EventLog eventLog = new EventLog {Source = "KPU_SERVICE", Log = "Application"};
+            eventLog.WriteEntry(message, entryType);
+        }
+
+    }
+}
